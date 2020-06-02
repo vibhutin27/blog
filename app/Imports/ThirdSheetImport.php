71a89aa30da4cb6a58d6a941ee05b210c10b44f3
vibhutin27 <vibhutin27@gmail.com>
2020-06-02 23:22:16 +0530

    excel import filter row

--- app/Imports/ThirdSheetImport.php
+++ app/Imports/ThirdSheetImport.php
@@ -15,12 +15,26 @@ class ThirdSheetImport implements  ToCollection,WithCalculatedFormulas,WithHeadi
 
     public function collection(Collection $myDataRows)
     {
-        foreach ($myDataRows as $dataRow){
+        $startcellrow = 3;
+        $endcellrow = 6;
+
+        
+        $user = auth()->user();
+        print($user->id);
+        print($user->name);
+        print($user->email);
+        foreach ($myDataRows as $startques => $dataRow)
+        {
+            if ($startques < $startcellrow || $startques >= $endcellrow)
+            {
+                continue;
+            } 
             $user   =  new UserUpload();
             $user->QNo =    $dataRow['qno'];
             $user->QText =  $dataRow['qtext'];
             $user->QValue = $dataRow['qvalue'];
+            $user->email =  auth()->user()->email;
             $user->save();
-        }
+        } 
     }
 }
